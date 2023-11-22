262658b2b543ae025f0746b7e34ccbbb886e1cae
Kateto19519 <146169304+Kateto19519@users.noreply.github.com>
2023-11-22 12:21:08 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,69 @@
+// Клас WebPage
+class WebPage {
+    String title;
+    String header;
+    String content;
+    String footer;
+}
+
+class WebPageBuilder {
+    WebPage webPage = new WebPage();
+
+    void setTitle(String title) {
+        webPage.title = title;
+    }
+
+    void setHeader(String header) {
+        webPage.header = header;
+    }
+
+    void setContent(String content) {
+        webPage.content = content;
+    }
+
+    void setFooter(String footer) {
+        webPage.footer = footer;
+    }
+
+    WebPage getResult() {
+        return webPage;
+    }
+}
+
+
+class WebPageDirector {
+    WebPageBuilder builder;
+
+    WebPageDirector(WebPageBuilder builder) {
+        this.builder = builder;
+    }
+
+    void createArticle(String content) {
+        builder.setTitle("Static Article");
+        builder.setHeader("Static Header");
+        builder.setContent(content);
+        builder.setFooter("Static Footer");
+    }
+
+    void createForm(String title, String fields) {
+        builder.setTitle(title);
+        builder.setHeader("Form Header");
+        builder.setContent("Form Fields: " + fields);
+        builder.setFooter("Form Footer");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        WebPageBuilder builder = new WebPageBuilder();
+        WebPageDirector director = new WebPageDirector(builder);
+
+        director.createArticle("Article Content");
+        WebPage articlePage = builder.getResult();
+        System.out.println("Article Page: " + articlePage.title + ", " + articlePage.header + ", " + articlePage.content + ", " + articlePage.footer);
+
+        director.createForm("Form Page", "Username, Password, Email");
+        WebPage formPage = builder.getResult();
+        System.out.println("Form Page: " + formPage.title + ", " + formPage.header + ", " + formPage.content + ", " + formPage.footer);
+    }
+}
